ea20e426d7b07f8d4f99b0550eb593be8a7918d3
Emeep <95995049+Emeep@users.noreply.github.com>
2023-05-15 14:17:47 +0700

    Add files via upload

---
+++ Timer Pass/main.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+import os
+
+import sys
+sys.path.insert(0, '') # Full path to MangaScroller folder
+import scrape # import functions of scrape.py
+
+path = 'MangaScroller\\Images'
+
+chapter = 7 # no. chapter u want to start reading 
+
+winwidth = 1000 # horizontal pixels
+fontsize = 20
+read_time = 5 # seconds
+
+# tkinter win init
+root = tk.Tk()
+frame = tk.Frame(root, width=300, height=300)
+page = tk.Label(frame, fg="black", font=("Helvetica", fontsize))
+timer = tk.Label(frame, fg="black", font=("Helvetica", fontsize))
+
+while True:
+    url = f'https://www.shounenabyss.com/manga/boys-abyss-chapter-{chapter}/' # URL of manga site
+
+    try: scrape.delete_images(path)
+    except: continue
+
+    scrape.main(url, path)
+    
+    for image in os.listdir(path):
+        imgpath = f'{path}\\{image}'
+        img = Image.open(imgpath)
+        zoom_percent = winwidth / img.size[0]
+        tkimg = ImageTk.PhotoImage(img.resize(tuple(int(i * zoom_percent) for i in img.size)))
+        width, height = tkimg.width(), tkimg.height()
+        root.geometry(f'{width}x{height + fontsize * 4}')
+
+        imgLabel = tk.Label(root, image=tkimg)
+        imgLabel.place(anchor='n', relx=0.5, y=fontsize * 3)
+
+        frame.place(anchor='n', relx=0.5, y=5)
+
+        page.config(text=f'ch {chapter} | {image} | time remaining:')
+        page.pack(side='left')
+
+        for i in range(read_time):
+            timer.config(text=f'{read_time - i}')
+            timer.pack(side='left')
+            root.after(1000, root.update())
+
+    chapter += 1
+    
